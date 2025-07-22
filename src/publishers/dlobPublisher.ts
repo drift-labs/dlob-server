2694e8163bfdbd9cf8bfa901d2fc1de684df3351
Jack Waller <jack.waller94@gmail.com>
2025-07-22 13:05:47 +1000

    chore: move ignore list to env var

--- src/publishers/dlobPublisher.ts
+++ src/publishers/dlobPublisher.ts
@@ -128,6 +128,7 @@ const SPOT_MARKETS_TO_LOAD =
 		: undefined;
 
 const enableOffloadQueue = process.env.ENABLE_OFFLOAD === 'true';
+const ignoreList = process.env.IGNORE_LIST?.split(',') || ['5N1AcdftujhXWZdBaqfciaKtXn6uVBKjmgwf6aQxR1vW'];
 
 logger.info(`RPC endpoint:  ${endpoint}`);
 logger.info(`WS endpoint:   ${wsEndpoint}`);
@@ -478,7 +479,7 @@ const main = async () => {
 		const orderSubscriber = new OrderSubscriberFiltered({
 			driftClient,
 			subscriptionConfig,
-			ignoreList: ['5N1AcdftujhXWZdBaqfciaKtXn6uVBKjmgwf6aQxR1vW']
+			ignoreList
 		});
 
 		dlobProvider = getDLOBProviderFromOrderSubscriber(orderSubscriber);
