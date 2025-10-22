b4a0675a0e8152f027af67f7fde54b86bf9bd04f
Nick Caradonna <lowkeynicc@gmail.com>
2025-10-22 12:31:55 -0400

    use MMOracleData to check vamm staleness

--- src/dlob-subscriber/DLOBSubscriberIO.ts
+++ src/dlob-subscriber/DLOBSubscriberIO.ts
@@ -343,7 +343,7 @@ export class DLOBSubscriberIO extends DLOBSubscriber {
 		const dlobSlot = this.slotSource.getSlot();
 		const oracleData =
 			marketType === 'perp'
-				? this.driftClient.getOracleDataForPerpMarket(marketArgs.marketIndex)
+				? this.driftClient.getMMOracleDataForPerpMarket(marketArgs.marketIndex)
 				: this.driftClient.getOracleDataForSpotMarket(marketArgs.marketIndex);
 		const oracleSlot = oracleData.slot;
 		const isPerpMarketAndPrelaunchMarket =
