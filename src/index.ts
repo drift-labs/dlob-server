463e082c45249ba0d3b6ae7bbc4a2fbb0518d5f0
lowkeynicc <85139158+lowkeynicc@users.noreply.github.com>
2025-11-19 14:54:45 -0500

    v2 params iteration (#555)

--- src/index.ts
+++ src/index.ts
@@ -1050,7 +1050,7 @@ const main = async (): Promise<void> => {
 				}
 			});
 
-			const inputParams = createMarketBasedAuctionParams(auctionParamsInput);
+			const inputParams = createMarketBasedAuctionParams(auctionParamsInput, undefined, apiVersion);
 
 			const result = await mapToMarketOrderParams(
 				inputParams,
